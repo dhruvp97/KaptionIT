1c46aa317233b490e47132672af35a5a6c1b97c3
Dhruv Patel <35431701+dhruvp97@users.noreply.github.com>
2019-01-26 14:33:39 -0500

    New Changes

--- test.py
+++ test.py
@@ -5,24 +5,58 @@ import os
 from google.cloud import vision
 from google.cloud.vision import types
 
-# Instantiates a client
-client = vision.ImageAnnotatorClient()
+def main(fileName):
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
 
-# The name of the image file to annotate
-file_name = os.path.join(
-    os.path.dirname(__file__),
-    'resources/k.jpg')
+    # The name of the image file to annotate
+    file_name = os.path.join(
+        os.path.dirname(__file__),
+        fileName)
 
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
 
-image = types.Image(content=content)
+    image = vision.types.Image(content=content)
 
-# Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
 
-print('Labels:')
-for label in labels:
-    print(label.description)
+    response2 = client.web_detection(image=image)
+    annotations = response.web_detection
+
+    print('Labels:')
+    for label in labels:
+        print(label.description)
+
+    detect_web(fileName)
+
+# Web Entities Results
+def detect_web(path):
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.web_detection(image=image)
+    annotations = response.web_detection
+
+    if annotations.best_guess_labels:
+        for label in annotations.best_guess_labels:
+            print('\nBest guess label: {}'.format(label.label))
+
+    if annotations.web_entities:
+        print('\n{} Web entities found: '.format(
+            len(annotations.web_entities)))
+
+        for entity in annotations.web_entities:
+            print('\n\tScore      : {}'.format(entity.score))
+            print(u'\tDescription: {}'.format(entity.description))
+
+# Driver
+main('resources\k2.jpg')
