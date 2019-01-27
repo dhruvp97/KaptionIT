1c0a13db98e666ddc4d19f07a866e78f67948a24
Dhruvin Balar <dhruvinb123@gmail.com>
2019-01-26 19:17:23 -0500

    frontend flask simple page working

--- bin/app.py
+++ bin/app.py
@@ -1,10 +1,7 @@
 import os
 from flask import Flask, render_template, request
-from sightengine.client import SightengineClient
 
 app = Flask(__name__)
-
-client = SightengineClient('{api_user}', '{api_secret}') # don't forget to add your credentials
 
 UPLOAD_FOLDER = os.path.basename('uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -16,32 +13,9 @@ def hello_world():
 @app.route('/upload', methods=['POST'])
 def upload_file():
     file = request.files['image']
-    filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
 
-    file.save(filename)
-    invalidImage = False
+    # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
+    file.save(f)
 
-    output = client.check('nudity', 'wad', 'celebrities', 'scam', 'face-attributes').set_file(filename)
-
-    # contains nudity
-    if output['nudity']['safe'] <= output['nudity']['partial'] and output['nudity']['safe'] <= output['nudity']['raw']:
-        invalidImage = True
-    # contains weapon, alcohol or drugs
-    if output['weapon'] > 0.2 or output['alcohol'] > 0.2 or output['drugs'] > 0.2:
-        invalidImage = True
-    # contains scammers
-    if output['scam']['prob'] > 0.85:
-        invalidImage = True
-    # contains celebrities
-    if 'celebrity' in output:
-        if output[0]['prob'] > 0.85:
-            invalidImage = True
-    # contains children
-    if 'attributes' in output:
-        if output['attributes']['minor'] > 0.85:
-            invalidImage = True
-
-    if invalidImage:
-        os.remove(filename)
-
-    return render_template('index.html', invalidImage=invalidImage, init=True)
+    return render_template('index.html')
